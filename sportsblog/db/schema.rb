60de831b253e94b9e86b272526c30d6a1f759ad1
Alberto Guadalupe <mralbertoguadalupe@gmail.com>
2019-01-04 17:28:43 -0500

    comments table now showing post ID and user ID associations

--- sportsblog/db/schema.rb
+++ sportsblog/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_04_210230) do
+ActiveRecord::Schema.define(version: 2019_01_04_222412) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,7 +21,9 @@ ActiveRecord::Schema.define(version: 2019_01_04_210230) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "posts_id"
+    t.bigint "user_id"
     t.index ["posts_id"], name: "index_comments_on_posts_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "posts", force: :cascade do |t|
@@ -40,5 +42,6 @@ ActiveRecord::Schema.define(version: 2019_01_04_210230) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "comments", "users"
   add_foreign_key "posts", "users"
 end
