40969e5a4e7bbe4abe0b7d8d4b82ba07318020b1
Alberto Guadalupe <mralbertoguadalupe@gmail.com>
2019-01-08 09:32:44 -0500

    header styling

--- sportsblog/client/src/components/nav/navbar.js
+++ sportsblog/client/src/components/nav/navbar.js
@@ -5,14 +5,17 @@ import { Icon } from "antd";
 export default function NavBar(props) {
   return (
     <header>
+    <a><img src={ require('../media/logo.webp')} style={{ height: "150px" }} alt={'home'}  /></a>
     <Icon
+      className = "profileicon"
       type="user"
       style={{ fontSize: "30px", color: "white" }}
 
       onClick={() => props.handleViewChange('Login')}
     />
-  
+
     </header>
 
           )
 }
+// onClick={() => this.setView('mainView')}
